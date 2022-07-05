4e877926708de5e39c2f5587a3155c970056982d
KhalidRouissi1 <khalidrouissi7@gmail.com>
2022-07-05 10:12:48 +0100

    Uploading The first steps

---
+++ script/script.js
@@ -0,0 +1,9 @@
+let image = document.querySelector(".image");
+
+
+function changeImage(phone){
+    image.src = phone;
+}
+function changeColor(color){
+    document.body.style.background = color;
+}
